d6bd1137cea2a114477797ab03b10401d76bc896
notyourbear <pracenberg@gmail.com>
2017-04-08 19:24:18 -0700

    finding stars

--- src/shared/utilFunctions.js
+++ src/shared/utilFunctions.js
@@ -11,11 +11,37 @@ export const selectRandom = (array) => {
   return array[place]
 }
 
-export const findAvailableSpaces = (board) => {
+export const findAvailableSpaces = (board, type = 'single') => {
   const values = Object.values(board)
-  return values.reduce((empties, cell) => {
-    return cell.type === 'empty' ? empties.concat([cell.location]) : empties
-  }, [])
+  switch (type) {
+    case 'star': {
+      const [height, width] = BOARD_DIMENSIONS
+      return values.reduce((empties, cell) => {
+        const [row, col] = cell.location
+        switch (true) {
+          case row < 1 || row > height - 2: return empties
+          case col < 1 || col > width - 2: return empties
+          default: {
+            const checks = [[-1, 0], [0, -1], [0, 0], [0, 1], [1, 0]]
+            const available = checks.reduce((options, check) => {
+              if (options.isAvailable === false) return options
+              const id = Cell.createId(row + check[0], col + check[1])
+              const locations = options.locations.concat(board[id].location)
+              const isAvailable = board[id].type !== 'snake' || board[id].type !== 'barrier'
+              return { isAvailable, locations }
+            }, {})
+
+            return !available.isAvailable ? empties : empties.concat([available.locations])
+          }
+        }
+      }, [])
+    }
+    default: {
+      return values.reduce((empties, cell) => {
+        return cell.type === 'empty' ? empties.concat([cell.location]) : empties
+      }, [])
+    }
+  }
 }
 
 export const findNextHeadLocation = (locationOfHead, direction, dimensions = BOARD_DIMENSIONS) => {
