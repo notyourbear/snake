4a929048a767ff2a8fae4edb11f1aced8dda0522
notyourbear <pracenberg@gmail.com>
2017-04-19 10:26:02 -0700

    trimmed some hedges

--- src/server/render-app.js
+++ src/server/render-app.js
@@ -9,7 +9,6 @@ const renderApp = title =>
         <title>${title}</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css">
-        <link rel="stylesheet" href="${STATIC_PATH}/css/style.css">
         <style>
           body { background: ${colors.BACKGROUND} }
         </style>
